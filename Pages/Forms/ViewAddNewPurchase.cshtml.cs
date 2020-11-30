510fbfa854016ae52f2a045d2c43155cd20b9a7b
nir kusan <nirkusan123@gmail.com>
2020-11-30 14:45:29 +0200

    Add project files.

---
+++ Pages/Forms/ViewAddNewPurchase.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MydorProject.Pages.Forms
+{
+    public class ViewAddNewPurchaseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
